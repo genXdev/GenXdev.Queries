b53468e983c5a4ea995081055dbefc744366b3ca
René Vaessen <rene.vaessen@gmail.com>
2025-09-16 13:37:52 +0200

    Release 1.268.2025

--- Functions/GenXdev.Queries.Websites/Open-ViralSimulation.ps1
+++ Functions/GenXdev.Queries.Websites/Open-ViralSimulation.ps1
@@ -2,7 +2,7 @@
 Part of PowerShell module : GenXdev.Queries.Websites
 Original cmdlet filename  : Open-ViralSimulation.ps1
 Original author           : René Vaessen / GenXdev
-Version                   : 1.264.2025
+Version                   : 1.268.2025
 ################################################################################
 MIT License
 
